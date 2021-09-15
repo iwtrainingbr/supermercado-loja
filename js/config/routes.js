5ecf39a16c833dbe835103ac0bfb905de627d073
samuellgc <samuellgc@hotmail.com>
2021-09-14 21:59:33 -0300

    criação da página de cadastro de produtos

--- js/config/routes.js
+++ js/config/routes.js
@@ -1,6 +1,6 @@
 const routes = {
   "addCategory": pageAddCategory,
   "listCategories": pageListCategories,
-
-  "listClients": pageListClients, 
+  "newProducts": pageNewProducts,
+  "listClients": pageListClients,
 };
