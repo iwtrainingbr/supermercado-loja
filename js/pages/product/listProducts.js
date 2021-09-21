52c7175cd703d5ff4fec70146e9c842eebf410a2
samuellgc <samuellgc@hotmail.com>
2021-09-21 20:50:13 -0300

    Add lightgallery na pagina de produtos

--- js/pages/product/listProducts.js
+++ js/pages/product/listProducts.js
@@ -2,7 +2,13 @@ function pageListProducts() {
   let data = [0, 1, 2, 3, 4, 5,6,7,8,9].map(prod => {
       return `
         <tr>
-          <td><img src="https://www.drogariaminasbrasil.com.br/media/product/a70/sabao-em-po-tixan-primavera-500g-050.jpg"width=60 height=40></td>
+          <td>
+            <div data-component="lightgallery">
+              <a href="https://www.drogariaminasbrasil.com.br/media/product/a70/sabao-em-po-tixan-primavera-500g-050.jpg" data-lg-size="1600-2400">
+                <img src="https://www.drogariaminasbrasil.com.br/media/product/a70/sabao-em-po-tixan-primavera-500g-050.jpg"width=60 height=40>
+              </a>
+            </div>
+          </td>
           <td>Teste ${prod}</td>
           <td>teste</td>
           <td>Teste</td>
@@ -35,5 +41,12 @@ function pageListProducts() {
         ${data}
       </tbody>
     </table>
+
+    <script type="text/javascript">
+      lightGallery(document.querySelector('[data-component="lightgallery"]'), {
+          plugins: [lgZoom, lgThumbnail],
+          speed: 500,
+      });
+    </script>
   `;
 }
