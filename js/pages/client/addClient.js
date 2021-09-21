687054143b8e622f65b56224d159ec421e517cad
La017 <luizm4artins@hotmail.com>
2021-09-21 20:59:03 -0300

    foi validado o formulário do cliente

--- js/pages/client/addClient.js
+++ js/pages/client/addClient.js
@@ -3,17 +3,17 @@ function pageAddClient (){
 <h1>Novo Cliente </h1>
 <form>
 
-<label for="name">Nome</label>
-<input class="form-control mb-3" id="name" placeholder="Ex:João Maria">
+<label for="name" >Nome</label>
+<input class="form-control mb-3" id="name" required placeholder="Ex:João Maria">
 
 <label for="CPF">CPF</label>
-<input class="form-control mb-3" id="CPF" type="text" placeholder="Ex:000.000.000-00">
+<input class="form-control mb-3" id="CPF" type="text" required placeholder="Ex:000.000.000-00">
 
 <label for="cep">Cep</label>
-<input class="form-control mb-3" id="cep" type="text" placeholder="Ex: 000.00.000">
+<input class="form-control mb-3" id="cep" type="text" required placeholder="Ex: 000.00.000">
 
 <label for="Endereço">Endereço</label>
-<input class="form-control mb-3" id="Endereço" placeholder="Ex:Rua: jurandir Leonel, N: / Bairro:">
+<input class="form-control mb-3" id="Endereço" required placeholder="Ex:Rua: jurandir Leonel, N: / Bairro:">
 
 
 <button class="btn btn-primary">ENVIAR</button>
