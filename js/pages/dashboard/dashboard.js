37863d6893d4e16ff43419864ad6c9b0ca503f1a
ualis0n <gabriel_fontenelle_@hotmail.com>
2021-09-21 20:33:37 -0300

    alteração da dash

--- js/pages/dashboard/dashboard.js
+++ js/pages/dashboard/dashboard.js
@@ -8,14 +8,14 @@ function pageDashboard() {
 <div class="row w-100">
         <div class="col-md-3">
             <div class="card border-info mx-sm-1 p-3">
-                <div class="card border-info shadow text-info p-3 my-card" ><span class="fa fa-list" aria-hidden="true"></span></div>
+                <div class="card border-info shadow text-info p-3 my-card" ><span class="fa fa-tags" aria-hidden="true"></span></div>
                 <div class="text-info text-center mt-3"><h4>Categorias</h4></div>
                 <div class="text-info text-center mt-2"><h1>234</h1></div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card border-success mx-sm-1 p-3">
-                <div class="card border-success shadow text-success p-3 my-card"><span class="fa fa-tags" aria-hidden="true"></span></div>
+                <div class="card border-success shadow text-success p-3 my-card"><span class="fa fa-shopping-cart" aria-hidden="true"></span></div>
                 <div class="text-success text-center mt-3"><h4>Produtos</h4></div>
                 <div class="text-success text-center mt-2"><h1>9332</h1></div>
             </div>
@@ -29,8 +29,8 @@ function pageDashboard() {
         </div>
         <div class="col-md-3">
             <div class="card border-warning mx-sm-1 p-3">
-                <div class="card border-warning shadow text-warning p-3 my-card" ><span class="fa fa-shopping-cart" aria-hidden="true"></span></div>
-                <div class="text-warning text-center mt-3"><h4>Inbox</h4></div>
+                <div class="card border-warning shadow text-warning p-3 my-card" ><span class="fa fa-users" aria-hidden="true"></span></div>
+                <div class="text-warning text-center mt-3"><h4>Clientes</h4></div>
                 <div class="text-warning text-center mt-2"><h1>346</h1></div>
             </div>
         </div>
