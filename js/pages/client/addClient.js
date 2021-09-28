f38b98df56fc95d74a29686fb9e31134b51fc0a4
Phytion1318 <danielvieiraperei@gmail.com>
2021-09-28 20:12:50 -0300

    Criado listar clientes

--- js/pages/client/addClient.js
+++ js/pages/client/addClient.js
@@ -1,24 +1,58 @@
+function saveClient () {
+  event.preventDefault();
+
+  let newClient = {
+    cpf: document.getElementById('cpf').value,
+    name: document.getElementById('name').value,
+
+    address: document.getElementById('address').value + " - " +  document.getElementById('cep').value,
+  }
+
+  let url = 'https://supermercado-front-08-default-rtdb.firebaseio.com/clients.json';
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(newClient),
+  });
+
+  alert('Novo veiculo cadastrado')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function pageAddClient (){
   return`
-<h1>Novo Cliente </h1>
-<form>
+  <h1>Novo Cliente </h1>
+  <form onsubmit="saveClient()" method="post">
 
-<label for="name" >Nome</label>
-<input class="form-control mb-3" id="name" required placeholder="Ex:João Maria">
+  <label for="name" >Nome</label>
+  <input class="form-control mb-3" id="name" required placeholder="Ex:João Maria">
 
-<label for="CPF">CPF</label>
-<input class="form-control mb-3" id="CPF" type="text" required placeholder="Ex:000.000.000-00">
+  <label for="cpf">CPF</label>
+  <input class="form-control mb-3" id="cpf" type="text" required placeholder="Ex:000.000.000-00">
 
-<label for="cep">Cep</label>
-<input class="form-control mb-3" id="cep" type="text" required placeholder="Ex: 000.00.000">
+  <label for="cep">Cep</label>
+  <input class="form-control mb-3" id="cep" type="text" required placeholder="Ex: 000.00.000">
 
-<label for="Endereço">Endereço</label>
-<input class="form-control mb-3" id="Endereço" required placeholder="Ex:Rua: jurandir Leonel, N: / Bairro:">
+  <label for="address">Endereço</label>
+  <input class="form-control mb-3" id="address" required placeholder="Ex:Rua: jurandir Leonel, N: / Bairro:">
 
 
-<button class="btn btn-primary">ENVIAR</button>
+  <button class="btn btn-primary">ENVIAR</button>
 
-</form>
+  </form>
 
   `;
 }
